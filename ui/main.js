533c8b357629a7bbe70f2724a1944d7a527751dd
Adiiigo <31741158+Adiiigo@users.noreply.github.com>
2017-09-08 12:52:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
 console.log('Loaded!');
+
+
+    //Change the text of the main-text div
+    var element = document.getElementById('mainElement');
+    
+    element.innerHTML = 'Hello, Changed from inside main.js' ;
