0e1fd59dbabadf952ffbcd5e907a86e284bbc5de
Adiiigo <31741158+Adiiigo@users.noreply.github.com>
2017-09-08 13:09:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,13 @@ console.log('Loaded!');
     
     //MOVE the Image
     var img = document.getElementById('madi');
-    img.onclick = function(){
-        img.style.margintop = '200px' ;
-    };
+    var marginLeft = 0 ;
+    function moveRight()
+    {
+        marginLeft = marginLeft + 10 ;
+        img.style.marginLeft = marginLeft + 'px' ;
+    }
+    img.onclick = function() {
+        var interval = setInterval(moveRight , 100) ;
+        };
     
