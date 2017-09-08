4dd89eee30e61ee67c77661151d6e90fd491611b
Adiiigo <31741158+Adiiigo@users.noreply.github.com>
2017-09-08 17:00:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -69,7 +69,7 @@ console.log('Loaded!');
       var list = '';
       for(var i = 0 ;i <names.length ; i++)
       {
-          list += '<li>' + name[i] + '</li>' ;
+          list += '<li>' + names[i] + '</li>' ;
       }
       var ul = document.getElementById('namelist') ;
       ul.innerHTML = list ;
